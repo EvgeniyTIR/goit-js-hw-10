9239e3d7c5511fe4a7d339c2cb8877656771557a
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-06-26 20:30:16 +0300

    statusText,Notify

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,16 +1,16 @@
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const options = 'fields=name,capital,population,flags,languages'
 
 export const fetchCountries = (name, options) => {
   return fetch(`https://restcountries.com/v3.1/name/${name}?${options}`)
     .then((res) => {
             if (!res.ok) {
-                throw new Error(res.status);
+                throw new Error(res.statusText);
             }
             return res.json();
         })
         .catch(error => {
             console.log(error);
-            //  Notify.failure('responce error in fetch');
+            Notify.failure('responce error in fetch');
         })
 };
